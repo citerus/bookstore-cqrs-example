a8801938e6687301ea660854d6848c69cdd31ff5
Jesper Hammarbäck <jesper.hammarback@gmail.com>
2014-09-01 21:31:27 +0200

    Split method in two.

--- application/src/test/java/se/citerus/cqrs/bookstore/application/TestDataImporter.java
+++ application/src/test/java/se/citerus/cqrs/bookstore/application/TestDataImporter.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.google.common.io.Resources;
 import org.junit.Ignore;
 import se.citerus.cqrs.bookstore.bookcatalog.api.BookDto;
-import se.citerus.cqrs.bookstore.infrastructure.JsonSerializer;
 import se.citerus.cqrs.bookstore.ordercontext.api.RegisterPublisherContractRequest;
 
 import java.util.List;
@@ -12,6 +11,7 @@ import java.util.List;
 import static com.google.common.base.Charsets.UTF_8;
 import static com.google.common.io.Resources.getResource;
 import static se.citerus.cqrs.bookstore.infrastructure.JsonSerializer.deserialize;
+import static se.citerus.cqrs.bookstore.infrastructure.JsonSerializer.serialize;
 
 @Ignore
 public class TestDataImporter {
@@ -19,38 +19,46 @@ public class TestDataImporter {
   private static final String SERVER_ADDRESS = "http://localhost:8080/service";
 
   public static void main(String[] args) {
-    importBooks();
+    importContracts("se/citerus/cqrs/bookstore/testdata/publishercontracts.json");
+    importBooks("se/citerus/cqrs/bookstore/testdata/books.json");
   }
 
-  private static void importBooks() {
-
+  private static void importContracts(String path) {
     try {
       TestHttpClient publisherClient = new TestHttpClient(SERVER_ADDRESS + "/publishercontract-requests").init();
 
-      // Add publisher contracts
-      String contractsJson = Resources.toString(getResource("se/citerus/cqrs/bookstore/testdata/publishercontracts.json"), UTF_8);
-      TypeReference<List<RegisterPublisherContractRequest>> listOfRegisterPublisherRequests = new TypeReference<List<RegisterPublisherContractRequest>>() {
-      };
-      List<RegisterPublisherContractRequest> publisherContractsRequests = deserialize(contractsJson, listOfRegisterPublisherRequests);
+      String contractsJson = Resources.toString(getResource(path), UTF_8);
+      TypeReference<List<RegisterPublisherContractRequest>> listOfRegisterPublisherRequests =
+          new TypeReference<List<RegisterPublisherContractRequest>>() {
+          };
 
-      for (RegisterPublisherContractRequest request : publisherContractsRequests) {
-        publisherClient.post(JsonSerializer.serialize(request));
+      List<RegisterPublisherContractRequest> requests = deserialize(contractsJson, listOfRegisterPublisherRequests);
+
+      for (RegisterPublisherContractRequest request : requests) {
+        publisherClient.post(serialize(request));
       }
-      System.out.println("Imported [" + publisherContractsRequests.size() + "] contracts...");
+      System.out.println("Imported [" + requests.size() + "] contracts...");
 
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private static void importBooks(String path) {
+    try {
       TestHttpClient bookClient = new TestHttpClient(SERVER_ADDRESS + "/books").init();
 
       // Add books
-      String booksJson = Resources.toString(getResource("se/citerus/cqrs/bookstore/testdata/books.json"), UTF_8);
+      String booksJson = Resources.toString(getResource(path), UTF_8);
       TypeReference<List<BookDto>> listOfCreateBookRequests = new TypeReference<List<BookDto>>() {
       };
 
-      List<BookDto> books = deserialize(booksJson, listOfCreateBookRequests);
+      List<BookDto> requests = deserialize(booksJson, listOfCreateBookRequests);
 
-      for (BookDto book : books) {
-        bookClient.post(JsonSerializer.serialize(book));
+      for (BookDto book : requests) {
+        bookClient.post(serialize(book));
       }
-      System.out.println("Imported [" + books.size() + "] books...");
+      System.out.println("Imported [" + requests.size() + "] books...");
 
     } catch (Exception e) {
       e.printStackTrace();
